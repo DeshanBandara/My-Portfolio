5655cb944a70f1dfc94b4fcf30356894e5ab0d57
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-07 18:36:54 +0530

    Added navbar

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../assest/logo.png'
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+        <img src={logo} alt='logo' className='logo' />
+        <div className='desktopMenu'>
+            <link></link>
+        </div>
+        <button className='desktopMenuButton'>
+            <img src='' alt='' className='desktopMenuImg'/> Contact Me
+        </button>
+    </nav>
   )
 }
 
