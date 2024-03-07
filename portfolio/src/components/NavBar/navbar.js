e30112787b73d1adb8964de09291be1cfb3b32fb
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-07 19:00:04 +0530

    Added navbar HTML codes and import components

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../../assest/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <img src={logo} alt='logo' className='logo' />
         <div className='desktopMenu'>
-            <link></link>
+            <Link className='DesktopMenuListItem'>Home</Link>
+            <Link className='DesktopMenuListItem'>Client</Link>
+            <Link className='DesktopMenuListItem'>Portfolio</Link>
+            <Link className='DesktopMenuListItem'>About</Link>
         </div>
         <button className='desktopMenuButton'>
             <img src='' alt='' className='desktopMenuImg'/> Contact Me
