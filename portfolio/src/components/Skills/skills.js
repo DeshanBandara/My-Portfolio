fb9922ea9e35f91c163031564af0c4ba4ac60756
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-08 22:05:30 +0530

    imported images

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './skills.css'
+import UIDesign from '../../assest/UIUX.png'
+import WebDesign from '../../assest/Web_design.png'
+import FrontEnd from '../../assest/frontend_develop.png'
+import DataBase from '../../assest/database.png'
+import AppDesign from '../../assest/app_design.png'
+import Backend from '../../assest/backend_development.png'
 
-const skills = () => {
+const Skills = () => {
   return (
-    <div>skills</div>
+    <section id='skills'>
+        <span className='skillTitle'>What I do</span>
+        <span className='skillDesc'></span>
+    </section>
   )
 }
 
-export default skills
+export default Skills
