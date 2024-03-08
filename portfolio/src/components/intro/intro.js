265b5ebe39853705ed306c432ae846764a065ae5
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-08 13:27:23 +0530

    Impoet btnImg and introName, introPara and btnImg

--- portfolio/src/components/intro/intro.js
+++ portfolio/src/components/intro/intro.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './intro.css'
 import bg from '../../assest/image.JPG'
+import btnImg from '../../assest/hire_me.png'
+import { Link } from 'react-scroll' //Link to a location
 
 
 const Intro = () => {
@@ -8,6 +10,9 @@ const Intro = () => {
     <section id='intro'>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
+            <span className='introText'>I'm <span className='introName'>Deshan Bandara</span> <br /> Full Stack Developer </span>
+            <p className='introPara'>I am a skilled Full-Stack Developer in creating <br />visually appealing and user friendly applications.</p>
+            <Link><button className='btn'><img src={ btnImg } className='btnImg' alt='Hire me' /> Hire Me</button></Link>
         </div>
         <img src={bg} alt="profile" className='bg' />
     </section>
