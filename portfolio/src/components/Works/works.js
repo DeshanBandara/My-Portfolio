4c08f889afcc15266ddbd1c4fae83e714a9be41c
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-11 17:35:23 +0530

    change images

--- portfolio/src/components/Works/works.js
+++ portfolio/src/components/Works/works.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './works.css'
-import My1 from '../../assest/my1.JPG'
-import My2 from '../../assest/my2.JPG'
-import My3 from '../../assest/my3.JPG'
-import My4 from '../../assest/my4.JPG'
-import My5 from '../../assest/my5.JPG'
-import My6 from '../../assest/my6.JPG'
+import My1 from '../../assest/my1.png'
+import My2 from '../../assest/my2.png'
+import My3 from '../../assest/my3.png'
+import My4 from '../../assest/my4.png'
+import My5 from '../../assest/my5.png'
+import My6 from '../../assest/my6.png'
 
 const Works = () => {
   return (
     <section id='works'>
       <h2 className='worksTitle'>My Portfolio</h2>
       <p className='worksDesc'>
-        I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited
-        to bring my skills and experience to help businesses achieve their goals and create a strong online presence.
+      I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited
+        to bring my skills and experience to help businesses achieve their goals and create a strong online presence. 
       </p>
       <div className='worksImgs'>
         <img src={My1} alt='' className='worksImg' />
