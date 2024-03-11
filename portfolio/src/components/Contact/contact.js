6d9d71b4ff882c3b79a9ae9551a5c8c2a1c1efd3
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-11 17:34:37 +0530

    added images

--- portfolio/src/components/Contact/contact.js
+++ portfolio/src/components/Contact/contact.js
@@ -14,6 +14,8 @@ import Insta from '../../assest/instagram.png'
 import YoutubeIcon from '../../assest/youtubeIcon.png'
 import Github from '../../assest/github.png'
 import emailjs from '@emailjs/browser';
+import ReactImg from '../../assest/react.png'
+import Mongodb from '../../assest/mongodb.png'
 
 const Contact = () => {
     const form = useRef();
@@ -41,16 +43,15 @@ const Contact = () => {
             <div id='clients'>
                 <h1 className='clientPageTitle'>My Clients</h1>
                 <p className='clientDesc'>
-                    I have had the opportunity to work with a divers group of companies.
-                    some of the notable companies I have worked with includes. 
+                    I use following technologies to fulfill your requirements
                 </p>
                 <div className='clientImgs'>
                     <img src={Adobe} alt='Adobe' className='clientImg' />
                     <img src={Git} alt='Git' className='clientImg' />
                     <img src={Canva} alt='Canva' className='clientImg' />
-                    <img src={Facebook} alt='Facebook' className='clientImg' />
+                    <img src={ReactImg} alt='React' className='clientImg' />
                     <img src={Figma} alt='Figma' className='clientImg' />
-                    <img src={Linkedin} alt='Linkedin' className='clientImg' />
+                    <img src={Mongodb} alt='Mongodb' className='clientImg' />
                     <img src={Microsoft} alt='Microsoft' className='clientImg' />
                     <img src={Youtube} alt='Youtube' className='clientImg' />
                 </div>
